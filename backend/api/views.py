ee803e5bfec13132495f5b624b74e85c96395be6
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-18 17:21:47 +0600

    view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 import json
 # from ..products.models import Product
 
@@ -26,8 +27,10 @@ from products.models import Product
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
-    data['id'] = model_data.id
-    data['title'] = model_data.title
-    data['price'] = model_data.price
-    data['content'] = model_data.content
+    if model_data:
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['price'] = model_data.price
+        # data['content'] = model_data.content
+        data = model_to_dict(model_data)
     return JsonResponse(data)
