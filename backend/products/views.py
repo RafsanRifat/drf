8fb102abd7a70c86599a5217139be7b7b6477be9
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-23 11:14:16 +0600

    CreateAPIView working

--- backend/products/views.py
+++ backend/products/views.py
@@ -10,10 +10,20 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-
+"""Detail API View"""
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+
+"""Create API View"""
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_create_view = ProductCreateAPIView.as_view()
---
+++ py_client/create.py
@@ -0,0 +1,8 @@
+import requests
+
+endpoint = "http://localhost:8000/api/products/"
+data = {"title": "generic title"}
+
+get_response = requests.post(endpoint, json=data)
+
+print(get_response.json())
