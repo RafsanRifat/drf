322f0adb48d9125e0956d42a1d6c714592aa9692
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-23 12:54:41 +0600

    perform_create working

--- backend/products/views.py
+++ backend/products/views.py
@@ -26,4 +26,13 @@ class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+
 product_create_view = ProductCreateAPIView.as_view()
