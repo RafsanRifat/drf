34d3fe4acdde9a94d558cf0fb95af0fe0d8cd628
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-28 13:16:21 +0600

    custom permission modified

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,5 +5,15 @@ from rest_framework.permissions import DjangoModelPermissions
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
     def has_permission(self, request, view):
         user = request.user
+        if user.is_staff:
+            if user.has_perm("products.add_product"):   # "appname.action_modelname"
+                return True
+            if user.has_perm("products.delete_product"):
+                return True
+            if user.has_perm("products.change_product"):
+                return True
+            if user.has_perm("products.view_product"):
+                return True
+            return False
         print(user.get_all_permissions())
-        return True
+        return False
