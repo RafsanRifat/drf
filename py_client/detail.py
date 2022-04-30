6fb658b5a9035f2195f96dfa2e22473103e085de
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-30 18:21:59 +0600

    py client updated

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,6 +1,8 @@
 import requests
+detail_id = input("Please enter the ID which one you want to know about : ")
+detail_id = int(detail_id)
 
-endpoint = "http://localhost:8000/api/products/1/"
+endpoint = f"http://localhost:8000/api/products/{detail_id}/"
 
 get_response = requests.get(endpoint)
 
--- py_client/update.py
+++ py_client/update.py
@@ -1,12 +1,26 @@
+from getpass import getpass
+
 import requests
 
-endpoint = "http://localhost:8000/api/products/1/update/"
+username = input("your username : ")
+password = getpass("your password : ")
+auth_endpoint = "http://localhost:8000/api/auth/"
+auth_response = requests.post(auth_endpoint, json={"username": username, "password": password})
+print(auth_response.json())
+token = auth_response.json()['token']
+print(token)
+
 
 data = {
     "title": "updated title"
 }
 
-get_response = requests.put(endpoint, json=data)
+headers = {
+        "Authorization": f"Token {token}"
+    }
+endpoint = "http://localhost:8000/api/products/1/update/"
+
+
+get_response = requests.put(endpoint, headers=headers, json=data )
 
 print(get_response.json())
-
