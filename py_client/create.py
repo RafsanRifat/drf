2f6841c6563e6b438fe7e418c4099be2cd77a4c8
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-30 16:14:33 +0600

    token added for create

--- py_client/create.py
+++ py_client/create.py
@@ -1,8 +1,23 @@
+from getpass import getpass
+
 import requests
 
-endpoint = "http://localhost:8000/api/products/"
-data = {"title": "generic title"}
+auth_endpoint = "http://localhost:8000/api/auth/"
+username = input("Your Username : ")
+password = getpass("Your Password : ")
 
-get_response = requests.post(endpoint, json=data)
+token_response = requests.post(auth_endpoint, json={"username": username, "password": password})
+if token_response.status_code == 200:
+    print(token_response.json())
+    token = token_response.json()["token"]
+    print(token)
+    endpoint = "http://localhost:8000/api/products/"
+    data = {"title": "new title is here"}
+    headers = {
+        "Authorization": f"Token {token}"
+    }
 
-print(get_response.json())
+    get_response = requests.post(endpoint, json=data, headers=headers)
+
+    print(get_response.json())
+print("your username or password are not matchcing")
