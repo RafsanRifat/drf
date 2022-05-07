7e0be34ce752ecd4957480a13fc475e1ea735691
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-07 12:40:21 +0600

    serializer url added

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework.reverse import reverse
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Product
         fields = [
             'id',
+            'pk',
+            'url',
             'title',
             'price',
             'content',
             'sale_price',
             'my_discount',
         ]
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse('product-detail', kwargs={"pk": obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         # print(obj.id)
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product_create_view),
-    path('<int:pk>/', views.product_detail_view),
-    path('<int:pk>/update/', views.product_update_view),
-    path('<int:pk>/delete/', views.product_delete_view)
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
+    path('<int:pk>/update/', views.product_update_view, name='product-update'),
+    path('<int:pk>/delete/', views.product_delete_view, name='product-delete')
 ]
