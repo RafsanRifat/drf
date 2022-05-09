209e58f4d22c08d0e7982f46de2f3247756a1e9c
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-09 18:28:29 +0600

    serializer modified

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,5 +3,9 @@ from rest_framework import serializers
 
 class UserPublicSerializer(serializers.Serializer):
     username = serializers.CharField(read_only=True)
-    this_is_not_real = serializers.CharField(read_only=True)
     id = serializers.IntegerField(read_only=True)
+    other_products = serializers.SerializerMethodField(read_only=True)
+
+    def get_other_products(self, obj):
+        print(obj)
+        return []
