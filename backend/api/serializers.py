39fdf584ae9a105c216aeba2c98ce3acea818464
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-09 17:38:37 +0600

    serializer added

---
+++ backend/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class UserPublicSerializer(serializers.Serializer):
+    username = serializers.CharField(read_only=True)
+    this_is_not_real = serializers.CharField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
