248a48b4d76b80aca365a98f4f92a3f4361011df
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-10 16:12:04 +0600

    new field and pagination added for products

--- backend/products/models.py
+++ backend/products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, default=99.99)
+    public = models.BooleanField(default=True)
 
     @property
     def sale_price(self):
